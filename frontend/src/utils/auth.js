beb4486830f5d2c73f1e9076d05a43734d35fd34
gorebogd <gorebogd@yandex.ru>
2021-03-26 00:22:23 +0300

    finished frontend

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,11 +1,11 @@
-export const BASE_URL = "https://auth.nomoreparties.co";
+import env from 'react-dotenv';
 
 function getResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
 }
 
 export function signUp(email, password) {
-  return fetch(`${BASE_URL}/signup`, {
+  return fetch(`${env.BASE_URL}/signup`, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -16,7 +16,7 @@ export function signUp(email, password) {
 }
 
 export function signIn(email, password) {
-  return fetch(`${BASE_URL}/signin`, {
+  return fetch(`${env.BASE_URL}/signin`, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -31,7 +31,7 @@ export function signIn(email, password) {
 }
 
 export function getLoginInfo(token) {
-  return fetch(`${BASE_URL}/users/me`, {
+  return fetch(`${env.BASE_URL}/users/me`, {
     method: "GET",
     headers: {
       Accept: "application/json",
